1151c58aa5c7c00ea157fed604b6e4a11142ec48
wesleyns <wesley.silva@systras.com.br>
2021-10-09 11:21:48 +0100

    first commit

---
+++ src/main/java/com/lojagraphql/lojagraphql/LojagraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.lojagraphql.lojagraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojagraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojagraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lojagraphql/lojagraphql/LojagraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojagraphql.lojagraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojagraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
