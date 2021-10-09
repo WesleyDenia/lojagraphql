dd022ca2abbcd4a32f97075cd6e360744841c965
Marcelo Pinheiro <marcelompinheiro@outlook.com>
2021-10-09 13:03:51 +0100

    configuring graphql with playground

--- src/main/java/com/lojagraphql/lojagraphql/resolver/category/GetCategoryResolver.java
+++ src/main/java/com/lojagraphql/lojagraphql/resolver/category/GetCategoryResolver.java
@@ -1,13 +1,13 @@
 package com.lojagraphql.lojagraphql.resolver.category;
 
 import com.lojagraphql.lojagraphql.domain.Category;
-import graphql.kickstart.tools.GraphQLResolver;
+import graphql.kickstart.tools.GraphQLQueryResolver;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
 @Component
-public class GetCategoryResolver implements GraphQLResolver {
+public class GetCategoryResolver implements GraphQLQueryResolver {
     public Category getCategory(UUID id) {
         return new Category(id, "Eletronicos");
     }
